74db3879f50fdbb4ede0cd9b7e3dd224babd7ecd
edualdan982 <wipitor982@gmail.com>
2023-09-07 00:26:59 -0400

    Probando implementar el servicio 2.1

---
+++ curso-kubernetes/msvc-auth/src/main/java/edual/spring/could/msvc/auth/MsvcAuthApplication.java
@@ -0,0 +1,13 @@
+package edual.spring.could.msvc.auth.msvcauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAuthApplication.class, args);
+	}
+
+}
