bfae4bafa31e40852fcf438e6b7ebc48006322e8
edsarmiento1 <edsarmiento1@umsa.bo>
2023-11-28 14:43:41 -0400

    Probando pkce con el Cors origin.

---
+++ spring-security/auth-client-social/src/main/java/com/security/authclientsocial/AuthClientSocialApplication.java
@@ -0,0 +1,13 @@
+package com.security.authclientsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthClientSocialApplication.class, args);
+	}
+
+}
